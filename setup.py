1ac36efa8ec91bf61c10a50b85c8ec6fd43799e9
Ben Rathbone <rath3.14159@protonmail.com>
2019-12-19 09:04:43 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "MorsePi",
+    version = "0.1.0",
+    author = "Ben Rathbone",
+    author_email = "rath3.14159@protonmail.com",
+    description = ("Files to transmit morse to a http endpoint and translate it from PWM lights back to english."),
+    license = "GNU GPL",
+    keywords = "Morse Code NeoPixels",
+)
